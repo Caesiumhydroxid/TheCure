97a2b8e0c3878accd960eca2525d7d372888f58c
Caesiumhydroxid <jonas.bodingbauer@gmail.com>
2016-03-01 16:26:25 +0100

    Added Stuff

--- core/src/com/jonas/thecuring/GameState.java
+++ core/src/com/jonas/thecuring/GameState.java
@@ -2,18 +2,18 @@ package com.jonas.thecuring;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -23,10 +23,11 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class GameState implements Screen {
 	
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
 	private TestFrame frame;
 	private Stage stage;
+	private AssetManager manager;
+	private TextButtonStyle smallButton;
+	private TextButtonStyle bigButton;
 	private TextButton a_button_0;
 	private TextButton a_button_1;
 	private TextButton a_button_2;
@@ -35,60 +36,56 @@ public class GameState implements Screen {
 	private TextButton a_button_5;
 	private TextButton a_button_6;
 	private Group attack;
-	public GameState(OrthographicCamera camera)
+	public GameState(OrthographicCamera camera,AssetManager manager)
 	{
-		this.camera = camera;
-		batch = new SpriteBatch();
+		this.manager = manager;
 		stage = new Stage(new FitViewport(Gdx.graphics.getWidth()/4, Gdx.graphics.getHeight()/4));
 		Gdx.input.setInputProcessor(stage);
 		attack = new Group();
 		frame = new TestFrame();
 		stage.addActor(frame);
-		TextButtonStyle style = new TextButtonStyle();
-		style.up = new TextureRegionDrawable(new TextureRegion(new Texture("Button.png")));
-		style.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button_Pressed.png")));
-		BitmapFont font = new BitmapFont(Gdx.files.internal("Font.fnt"), Gdx.files.internal("Font.png"),false );
-		style.font = font;
-		style.downFontColor = new Color(0.5f,0.5f,0.5f,1);
-		style.fontColor = Color.BLACK;
-		a_button_0 = new TextButton("Aggressiv",style);
-		a_button_0.addListener(new ChangeListener() {
-			
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				System.out.println(event);
-			}
-		});
+		TextButtonStyle bigButton = new TextButtonStyle();
+		
+		BitmapFont font = manager.get("Font.fnt");
+		bigButton.up = new TextureRegionDrawable(new TextureRegion((Texture) manager.get("Button.png")));
+		bigButton.down = new TextureRegionDrawable(new TextureRegion((Texture) manager.get("Button_Pressed.png")));
+		bigButton.font = font;
+		bigButton.downFontColor = new Color(0.5f,0.5f,0.5f,1);
+		bigButton.fontColor = Color.BLACK;
+		
+		a_button_0 = new TextButton("Aggressiv",bigButton);
 		a_button_0.setPosition(46, 119-13);
 		attack.addActor(a_button_0);
 		
-		a_button_1 = new TextButton("Mehr \nBlutkörper",style);
+		a_button_1 = new TextButton("Mehr \nBlutkï¿½rper",bigButton);
 		a_button_1.setPosition(5, 119-43);
 		
 		attack.addActor(a_button_1);
 		
-		a_button_2 = new TextButton("Mutationen",style);
+		a_button_2 = new TextButton("Mutationen",bigButton);
 		a_button_2.setPosition(82, 119-43);
 		attack.addActor(a_button_2);
 		
-		a_button_3 = new TextButton("schnelle \nZellteilung",style);
+		a_button_3 = new TextButton("schnelle \nZellteilung",bigButton);
 		a_button_3.setPosition(5, 119-69);
 		attack.addActor(a_button_3);
 		
-		a_button_4 = new TextButton("gefährliche\nMutationen",style);
+		a_button_4 = new TextButton("gefï¿½hrliche\nMutationen",bigButton);
 		a_button_4.setPosition(82, 119-69);
 		attack.addActor(a_button_4);
 		
-		a_button_5 = new TextButton("Metastasen",style);
+		a_button_5 = new TextButton("Metastasen",bigButton);
 		a_button_5.setPosition(44, 119-94);
 		attack.addActor(a_button_5);
 		
-		a_button_6 = new TextButton("greift\nGewebe an",style);
+		a_button_6 = new TextButton("greift\nGewebe an",bigButton);
 		a_button_6.setPosition(44, 119-119);
 		attack.addActor(a_button_6);
 		
 		attack.setPosition(18, 180-143);
 		stage.addActor(attack);
+		
+	
 	}
 
 	@Override
@@ -98,12 +95,9 @@ public class GameState implements Screen {
 	}
 	@Override
 	public void render(float delta) {
-		
-	
+		attack.act(delta);
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		
 		stage.draw();
 	
 
--- core/src/com/jonas/thecuring/TheCuring.java
+++ core/src/com/jonas/thecuring/TheCuring.java
@@ -1,9 +1,12 @@
 package com.jonas.thecuring;
 
-import com.badlogic.gdx.Game;
+import java.awt.Font;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TheCuring extends Game {
@@ -11,6 +14,7 @@ public class TheCuring extends Game {
 	Texture img;
 	OrthographicCamera camera;
 	GameState gameState;
+	AssetManager assetManager;
 	Game game;
 	@Override
 	public void create () {
@@ -18,7 +22,13 @@ public class TheCuring extends Game {
 		camera = new OrthographicCamera(256,144);
 		camera.position.set(camera.viewportWidth/2.f,camera.viewportHeight/2.f,0);
 		camera.update();
-		gameState = new GameState(camera);
+		assetManager = new AssetManager();
+		assetManager.load("Button_Pressed.png", Texture.class);
+		assetManager.load("Button.png",Texture.class);
+		assetManager.load("TestGame.png",Texture.class);
+		assetManager.load("Font.fnt",BitmapFont.class);
+		assetManager.finishLoading();
+		gameState = new GameState(camera,assetManager);
 		game.setScreen(gameState);
 	}
 
